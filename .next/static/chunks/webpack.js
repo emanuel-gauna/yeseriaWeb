53aa9902018aef64f1b6e81a2f016ff9e249fc01
emanuel-gauna <lemagauna@gmail.com>
2025-09-03 18:58:16 -0300

    Cleaned up dependencies for Next.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1d75de745ac146e"; }
+/******/ 		__webpack_require__.h = function() { return "e144968012a5bcdd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.9fddd50523d8cc55.hot-update.js
+++ .next/static/webpack/webpack.9fddd50523d8cc55.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "767aacecec6bc6d4"; }
+/******/ 	__webpack_require__.h = function() { return "e144968012a5bcdd"; }
 /******/ }();
 /******/ 
 /******/ }
