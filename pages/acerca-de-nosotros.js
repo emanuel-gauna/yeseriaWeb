b66ec18d9724595e1a2bd26f3c06be9a38d27cb2
emanuel-gauna <lemagauna@gmail.com>
2025-09-04 17:55:09 -0300

    agegando metadatos para el seo4

--- pages/acerca-de-nosotros.js
+++ pages/acerca-de-nosotros.js
@@ -6,17 +6,18 @@ import StickyButtons from '../src/components/StickyButtons';
 export default function Acerca() {
   return (
     <>
-      <Head>
-        <title>Acerca de Emanuel Gauna | Yesero, Pintor y Albañil Profesional</title>
-        <meta
-          name="description"
-          content="Emanuel Gauna ofrece servicios profesionales de yesería, pintura y albañilería, especializado en microcemento y remodelaciones en Buenos Aires y AMBA. Garantía de calidad y atención personalizada."
-        />
-        <meta
-          name="keywords"
-          content="yesero, pintor, albañil, microcemento, remodelaciones, yesería, pintura, revestimientos, Buenos Aires, AMBA"
-        />
-      </Head>
+     
+      <Head>
+        <title>Acerca de Emanuel Gauna | Yesero, Pintor y Albañil Profesional</title>
+        <meta
+          name="description"
+          content="Emanuel Gauna ofrece servicios profesionales de yesería, pintura y albañilería, especializado en microcemento y remodelaciones en Buenos Aires y AMBA. Garantía de calidad y atención personalizada."
+        />
+        <meta
+          name="keywords"
+          content="yesero, pintor, albañil, microcemento, remodelaciones, yesería, pintura, revestimientos, Buenos Aires, AMBA"
+        />
+      </Head>
 
       <main className="min-h-screen">
         <FondoLogo />
