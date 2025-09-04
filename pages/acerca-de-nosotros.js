b948bf0ffcb1824f337dcf8bd099c386e879f4be
emanuel-gauna <lemagauna@gmail.com>
2025-09-04 18:00:08 -0300

    agegando metadatos para el seo5

--- pages/acerca-de-nosotros.js
+++ pages/acerca-de-nosotros.js
@@ -4,26 +4,37 @@ import FondoLogo from '../src/components/FondoLogo';
 import StickyButtons from '../src/components/StickyButtons';
 
 export default function Acerca() {
-  return (
-    <>
-     
+  return (
+    <>
       <Head>
         <title>Acerca de Emanuel Gauna | Yesero, Pintor y Albañil Profesional</title>
         <meta
           name="description"
-          content="Emanuel Gauna ofrece servicios profesionales de yesería, pintura y albañilería, especializado en microcemento y remodelaciones en Buenos Aires y AMBA. Garantía de calidad y atención personalizada."
+          content="Servicios profesionales de yesería, pintura, albañilería y microcemento. Garantía de calidad en remodelaciones en Buenos Aires y AMBA."
         />
         <meta
           name="keywords"
           content="yesero, pintor, albañil, microcemento, remodelaciones, yesería, pintura, revestimientos, Buenos Aires, AMBA"
         />
+        {/* Open Graph y Twitter para redes sociales */}
+        <meta property="og:title" content="Acerca de Emanuel Gauna | Yesero y Pintor Profesional" />
+        <meta property="og:description" content="Servicios de yesería, pintura, albañilería y microcemento en Buenos Aires y AMBA. Contacto: 11 6437-1277." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yeseria-web.vercel.app/acerca-de-nosotros" />
+        <meta property="og:image" content="/logoyeso.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Acerca de Emanuel Gauna | Yesero y Pintor Profesional" />
+        <meta name="twitter:description" content="Servicios de yesería, pintura, albañilería y microcemento en Buenos Aires y AMBA. Contacto: 11 6437-1277." />
+        <meta name="twitter:image" content="/logoyeso.png" />
+        {/* Para evitar el error de la etiqueta de idioma */}
+        <meta httpEquiv="content-language" content="es-ar" />
       </Head>
 
-      <main className="min-h-screen">
-        <FondoLogo />
-        <AcercaComponent />
-      </main>
-      <StickyButtons />
-    </>
-  );
+      <main className="min-h-screen">
+        <FondoLogo />
+        <AcercaComponent />
+      </main>
+      <StickyButtons />
+    </>
+  );
 }
